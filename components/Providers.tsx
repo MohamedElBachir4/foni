9a529162227684d4e62537f11ccac851ee7821f2
Mohammed Bachir <souatimohamed47@gmail.com>
2026-06-02 14:12:47 +0100

    وصف التحديث

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -2,14 +2,12 @@
 
 import { CartProvider } from "@/context/CartContext";
 import { AccountProvider } from "@/context/AccountContext";
-import { RegisterPromptBanner } from "@/components/RegisterPromptBanner";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <AccountProvider>
       <CartProvider>
         {children}
-        <RegisterPromptBanner />
       </CartProvider>
     </AccountProvider>
   );
